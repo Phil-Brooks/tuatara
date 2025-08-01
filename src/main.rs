21ace004a9d004829d0472a3e5aa3a52c25e7a33
Phil-Brooks <phil@brookshome.co.uk>
2025-08-01 20:50:06 +0100

    print_bitboard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+fn print_bitboard(bitboard: u64) {
+    const LAST_BIT: u64 = 63;
+    for rank in 0..8 {
+        for file in (0..8).rev() {
+            let mask = 1u64 << (LAST_BIT - (rank * 8) - file);
+            let char = if bitboard & mask != 0 { '1' } else { '0' };
+            print!("{char} ");
+        }
+        println!();
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    print_bitboard(1);
+    println!();
+    print_bitboard(2);
+    println!();
+    print_bitboard(3);
 }
